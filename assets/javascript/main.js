fd169babc6b291e7952cd27f8b05370b8fb0afb7
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-11 12:46:15 -0500

    Inital javascript, creating player to Firebase

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,4 +2,33 @@ $(document).ready(function () {
 
     var db = firebase.database();
 
-})
+    //var playerOneWins;
+    //var playerOneLosses;
+    //var playerOneName;
+    //var playerTwoWins;
+    //var playerTwoLosses;
+    //var PlayerTwoName;
+    var playerTie;
+
+    var pOneDb;
+    var pTwoDb;
+
+    $(document).on('click', makePlayer);
+
+    function makePlayer() {
+
+        playerName = 'Chuck'
+
+        db.ref('one').set({
+            name: playerName,
+            wins: 0,
+            losses: 0,
+            ties: 0,
+        })
+
+        pOneDb = db.ref('one');
+
+
+    }
+
+});
