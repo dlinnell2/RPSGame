aff5c81ce1730f9a6abdb5350f7ae36e0a78acdd
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-14 15:56:59 -0500

    figured out the two realities

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
 
     var db = firebase.database();
 
-    var playerOne;
-    var playerTwo;
     var playerTie;
 
     var playerOneChoice;
     var playerTwoChoice;
 
-    var playOneExist;
-    var playTwoExist;
+    var playerOne;
+    var playerTwo;
+
+    var playerName;
 
     var turn;
 
@@ -24,183 +24,191 @@ $(document).ready(function () {
 
         playerTwo = snap.child('/two').val();
 
+        console.log(playOneExist);
+
     }, function (errorObject) {
 
         console.log("The read failed: " + errorObject.code);
 
     });
 
+
     // Making new players
     function makePlayer() {
 
         event.preventDefault();
 
-        if (playOneExist && playTwoExist) {
+        if (!(playOneExist && playTwoExist)) {
 
-            $('#outcome').text('Let\'s begin!');
+            if (!playOneExist) {
 
-            setTimeout(gamePlayerOne, 1000);
+                playerName = $('#newPlayer').val().trim();
 
-        } else if (playOneExist) {
+                db.ref('/one').set({
+                    name: playerName,
+                    wins: 0,
+                    losses: 0,
+                    ties: 0,
+                });
 
-            var playerName = $('#newPlayer').val().trim();
+                db.ref('/one').onDisconnect().remove();
 
-            db.ref('/two').set({
-                name: playerName,
-                wins: 0,
-                losses: 0,
-                ties: 0,
-            });
+            } else if (!playTwoExist) {
 
-            $('#outcome').text('Let\'s begin!');
+                playerName = $('#newPlayer').val().trim();
 
-            setTimeout(gamePlayerOne, 1000);
+                db.ref('/two').set({
+                    name: playerName,
+                    wins: 0,
+                    losses: 0,
+                    ties: 0,
+                });
 
-        } else {
+                db.ref('/two').onDisconnect().remove();
 
-            var playerName = $('#newPlayer').val().trim();
-
-            db.ref('/one').set({
-                name: playerName,
-                wins: 0,
-                losses: 0,
-                ties: 0,
-            });
-
-        };
+            };
 
 
-    }
-
-    // Running of game for player One
-    function gamePlayerOne() {
-
-        // Set text for player one's choices
-        $('#rockOne').text('rock');
-        $('#paperOne').text('paper');
-        $('#scissorsOne').text('scissors')
-
-        // Show the choices
-        $('.choiceOne').show();
-        $('#waitingOne').hide();
-
-        // Set text for player two's status
-        $('#waitingTwo').text('Waiting on ' + playerOne.name);
-
-        // Show the status
-        $('#waitingTwo').show();
-        $('.choiceTwo').hide();
-
-        // Once player one has clicked their selection
-        $('.choiceOne').on('click', function () {
-
-            playerOneChoice = $(this).text();
-
-            gamePlayerTwo();
-
-        })
-
-    }
-
-    // Running of game for player Two
-    function gamePlayerTwo() {
-
-        // Set text for player two's choices
-        $('#rockTwo').text('rock');
-        $('#paperTwo').text('paper');
-        $('#scissorsTwo').text('scissors')
-
-        // Show the choices
-        $('.choiceTwo').show();
-        $('#waitingTwo').hide();
-
-        // Set text for player two's status
-        $('#waitingOne').text('Waiting on ' + playerTwo.name);
-
-        // Show the status
-        $('#waitingOne').show();
-        $('.choiceOne').hide();
-
-        // Once player one has clicked their selection
-        $('.choiceTwo').on('click', function () {
-
-            playerTwoChoice = $(this).text();
-
-            outcome();
-
-        });
-
-    };
-
-    function playerOneWins() {
-
-        // Update player one's wins on database
-        var playWins = playerOne.wins;
-        playWins++
-        db.ref().child('/one/wins').set(playWins);
-
-        // Update player two's loss on database
-        var playLoss = playerTwo.losses;
-        playLoss++
-        db.ref().child('/two/losses').set(playLoss);
-
-    }
-
-    function playerTwoWins() {
-
-        // Update player two's wins on database
-        var playWins = playerTwo.wins;
-        playWins++
-        db.ref().child('/two/wins').set(playWins);
-
-        // Update player one's loss on database
-        var playLoss = playerOne.losses;
-        playLoss++
-        db.ref().child('/one/losses').set(playLoss);
-    }
-
-    // Evaluate a winner
-    function outcome() {
-
-        $('#waitingOne, .choiceTwo').hide();
-
-        if (playerOneChoice === playerTwoChoice) {
-
-            console.log('tie')
-
-        } else if (playerOneChoice === 'rock') {
-
-            if (playerTwoChoice === 'scissors') {
-                playerOneWins();
-
-            } else if (playerTwoChoice === 'paper') {
-                playerTwoWins(); 
-            }
-
-        } else if (playerOneChoice === 'scissors') {
-
-            if (playerTwoChoice === 'paper') {
-                playerOneWins();
-
-            } else if (playerTwoChoice === 'rock') {
-                playerTwoWins();
-
-            }
-
-        } else if (playerOneChoice === 'paper') {
-
-            if (playerTwoChoice === 'rock') {
-                playerOneWins();
-
-            } else if (playerTwoChoice === 'scissors') {
-                playerTwoWins();
-                
-            }
         }
 
     };
 
+    /*     // Running of game for player One
+        function gamePlayerOne() {
+    
+            // Set text for player one's choices
+            $('#rockOne').text('rock');
+            $('#paperOne').text('paper');
+            $('#scissorsOne').text('scissors')
+    
+            // Show the choices
+            $('.choiceOne').show();
+            $('#waitingOne').hide();
+    
+            // Set text for player two's status
+            $('#waitingTwo').text('Waiting on ' + playerOne.name);
+    
+            // Show the status
+            $('#waitingTwo').show();
+            $('.choiceTwo').hide();
+    
+            // Once player one has clicked their selection
+            $('.choiceOne').on('click', function () {
+    
+                playerOneChoice = $(this).text();
+    
+                gamePlayerTwo();
+    
+            })
+    
+        }
+    
+        // Running of game for player Two
+        function gamePlayerTwo() {
+    
+            // Set text for player two's choices
+            $('#rockTwo').text('rock');
+            $('#paperTwo').text('paper');
+            $('#scissorsTwo').text('scissors')
+    
+            // Show the choices
+            $('.choiceTwo').show();
+            $('#waitingTwo').hide();
+    
+            // Set text for player two's status
+            $('#waitingOne').text('Waiting on ' + playerTwo.name);
+    
+            // Show the status
+            $('#waitingOne').show();
+            $('.choiceOne').hide();
+    
+            // Once player one has clicked their selection
+            $('.choiceTwo').on('click', function () {
+    
+                playerTwoChoice = $(this).text();
+    
+                outcome();
+    
+            });
+    
+        };
+    
+        function playerOneWins() {
+    
+            // Update player one's wins on database
+            var playWins = playerOne.wins;
+            playWins++
+            db.ref().child('/one/wins').set(playWins);
+    
+            // Update player two's loss on database
+            var playLoss = playerTwo.losses;
+            playLoss++
+            db.ref().child('/two/losses').set(playLoss);
+    
+        }
+    
+        function playerTwoWins() {
+    
+            // Update player two's wins on database
+            var playWins = playerTwo.wins;
+            playWins++
+            db.ref().child('/two/wins').set(playWins);
+    
+            // Update player one's loss on database
+            var playLoss = playerOne.losses;
+            playLoss++
+            db.ref().child('/one/losses').set(playLoss);
+        }
+    
+        // Evaluate a winner
+        function outcome() {
+    
+            $('#waitingOne, .choiceTwo').hide();
+    
+            if (playerOneChoice === playerTwoChoice) {
+    
+                console.log('tie');
+    
+            } else if (playerOneChoice === 'rock') {
+    
+                if (playerTwoChoice === 'scissors') {
+                    playerOneWins();
+    
+                } else if (playerTwoChoice === 'paper') {
+                    playerTwoWins(); 
+    
+                };
+    
+            } else if (playerOneChoice === 'scissors') {
+    
+                if (playerTwoChoice === 'paper') {
+                    playerOneWins();
+    
+                } else if (playerTwoChoice === 'rock') {
+                    playerTwoWins();
+    
+                };
+    
+            } else if (playerOneChoice === 'paper') {
+    
+                if (playerTwoChoice === 'rock') {
+                    playerOneWins();
+    
+                } else if (playerTwoChoice === 'scissors') {
+                    playerTwoWins();
+    
+                };
+            };
+    
+        }; */
+
     $('#makePlayer').on('click', makePlayer);
+
+    $('.player').on('click', function(){
+        if (playerName === playerOne.name) {
+            console.log('success');
+        };
+    });
 
 
 });
